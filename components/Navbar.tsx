d64c83c03a00e34b639ef1c3f69392cdf5695d5a
a-manan123 <manankhokhar1278@gmail.com>
2025-09-22 23:28:03 +0500

    Implemented Auth, Billing, Database Setup, Create Companion Server Action

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import {
+    SignInButton,
+    SignedIn,
+    SignedOut,
+    UserButton,
+} from '@clerk/nextjs'
 import NavItems from "@/components/NavItems";
 
 
-const Navbar = () => {
+const  Navbar = () => {
     return (
         <nav className="navbar">
             <Link href='/'>
@@ -18,7 +24,16 @@ const Navbar = () => {
             </Link>
             <div className='flex items-center gap-8'>
                 <NavItems/>
-                <p>Sign In</p>
+                <SignedOut>
+                    <SignInButton>
+                        <button className='btn-signin'>
+                            Sign In
+                        </button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <UserButton/>
+                </SignedIn>
             </div>
         </nav>
     )
